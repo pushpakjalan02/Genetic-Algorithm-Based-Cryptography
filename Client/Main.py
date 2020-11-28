cff30866c5de501d2136dd6771938496175b98cf
Pushpak Jalan <pushpakjalan07@outlook.com>
2020-11-28 12:18:39 +0530

    Added Client/Server Communication

--- Client/Main.py
+++ Client/Main.py
@@ -1,7 +1,40 @@
+import socket
+import Encrypt
 
-from Encrypt import *
+def sendData(cipherText, key):
+
+    ip = input('Enter Server IP Address: ')
+    port = int(input('Enter Server Port: '))
+    print()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Created socket successfully\n')
+
+    s.connect((ip, port))
+    print('Connection to server established\n' + 
+        'Server IP Address: ' + ip + '\n' + 
+        'Server Port: ' + str(port) + '\n')
+
+    s.sendall(cipherText.encode('utf-8'))
+    print('Sent Data: ' + cipherText)
+    print('Decryption key: ' + key + '\n')
+
+    s.close()
+    print('Connection closed\n')
+
+    return
 
 def main():
+
+    print()
+    plainText = input('Enter data to send: ')
+    print()
+
+    cipherText, key = Encrypt.encrypt(plainText)
+    print('Done encrypting\n')
+
+    sendData(cipherText, key)
+
     return
 
 if __name__ == "__main__":
--- Server/Main.py
+++ Server/Main.py
@@ -1,7 +1,44 @@
+import socket
+import Decrypt
 
-from Encrypt import *
+def createSocket():
+
+    print()
+    ip = input('Enter IP Address: ')
+    port = int(input('Enter Port: '))
+    print()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Created Socket Successfully\n')
+
+    s.bind((ip, port))
+    print('Binding done to port ' + str(port) + '\n')
+
+    s.listen(5)
+    print('Started listening\n')
+
+    return s
 
 def main():
+    s = createSocket()
+    while(True):
+        c, addr = s.accept()
+        print('Connection established with ' + str(addr) + '\n')
+
+        cipherText = c.recv(1024).decode('utf-8')
+        print('Received data: ' + cipherText + '\n')
+
+        key = input('Enter Decryption Key: ')
+        print()
+
+        decryptedData = Decrypt.decrypt(cipherText, key)
+        print('Done decrypting\n')
+        
+        print('Decrypted data: ' + decryptedData + '\n')
+
+        c.close()
+        print('Connection closed\n')
+
     return
 
 if __name__ == "__main__":
